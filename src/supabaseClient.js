c4335672dc0b13482f8c701f2a0c8a936b6a57eb
Ansper <kasparov04@list.ru>
2025-04-21 11:00:58 +0300

    test

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
+alert(process.env.REACT_APP_SUPABASE_URL);
+
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
